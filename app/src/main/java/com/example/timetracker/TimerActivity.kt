e6e56111bb8f50575a1989bdc5ac5afc4249ac38
Stephen Yang <stepheny755@gmail.com>
2019-11-05 14:37:04 -0500

    this didn't work.

--- app/src/main/java/com/example/timetracker/TimerActivity.kt
+++ app/src/main/java/com/example/timetracker/TimerActivity.kt
@@ -12,6 +12,21 @@ import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.alertdialog.view.*
+import android.R.menu
+import android.view.MenuInflater
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
 
 class TimerActivity : AppCompatActivity() {
 
@@ -26,7 +41,7 @@ class TimerActivity : AppCompatActivity() {
     private var lastEvent:String?= null
     private var newEvent:Event?=null
 
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     //private val
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,10 +88,33 @@ class TimerActivity : AppCompatActivity() {
             chronometer.base = SystemClock.elapsedRealtime()
             state=State.Stopped
         }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_share, R.id.nav_send
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
